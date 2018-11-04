1ae2d322d3f9bce6e5e21a1028fead6c44d3231c
yaoxs1996 <infinity_yao@hotmail.com>
2018-11-04 19:48:04 +0800

    Delete same element

---
+++ DeleteSame.cpp
@@ -0,0 +1,44 @@
+//Delete same elements from sequence list
+//从有序线性表中删除
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool deleteSame(vector<int> &L)
+{
+    if(L.size() == 0)
+        return false;
+
+    int i, j;
+    for(i=0, j=1; j<L.size(); j++)
+    {
+        if(L[i] != L[j])
+        {
+            L[++i] = L[j];
+        }
+    }
+
+    //删除后序的重复序列
+    for(j=0; j<=i; j++)
+        L.pop_back();
+
+    return true;
+}
+
+int main()
+{
+    //有序线性表
+    vector<int> list {1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6};
+
+    cout << "Before delete" << endl;
+    for_each(list.begin(),list.end(),[](int x){cout << x << " ";});
+    
+    deleteSame(list);
+    
+    cout << endl << "After delete" << endl;
+    for_each(list.begin(),list.end(),[](int x){cout << x << " ";});
+
+    system("pause");
+}
